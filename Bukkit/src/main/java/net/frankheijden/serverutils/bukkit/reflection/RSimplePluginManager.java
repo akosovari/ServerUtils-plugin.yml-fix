54a3e205389c0be7163966bee14492dc7e0d8cca
Siroshun09 <siro.shun8@gmail.com>
2023-02-21 05:16:07 +0900

    fix: plugin loading/unloading/reloading after Paper 1.19.3 build 405
    This is TEMPORALLY FIX so don't open PR to original repository

--- Bukkit/src/main/java/net/frankheijden/serverutils/bukkit/reflection/RSimplePluginManager.java
+++ Bukkit/src/main/java/net/frankheijden/serverutils/bukkit/reflection/RSimplePluginManager.java
@@ -1,28 +1,61 @@
 package net.frankheijden.serverutils.bukkit.reflection;
 
 import dev.frankheijden.minecraftreflection.MinecraftReflection;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.regex.Pattern;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.SimplePluginManager;
 
 public class RSimplePluginManager {
 
     private static final MinecraftReflection reflection = MinecraftReflection.of(SimplePluginManager.class);
+    private static final Object PAPER_INSTANCE_MANAGER;
+    private static final Field PLUGINS_FIELD;
+    private static final Field LOOKUP_NAMES_FIELD;
+
+    static {
+        Object paperInstanceManager;
+        Field pluginsField;
+        Field lookupNamesField;
+
+        try {
+            Object paperPluginManager = RCraftServer.getReflection().get(Bukkit.getServer(), "paperPluginManager");
+            Field instanceManagerField = paperPluginManager.getClass().getDeclaredField("instanceManager");
+            instanceManagerField.setAccessible(true);
+            paperInstanceManager = instanceManagerField.get(paperPluginManager);
+            pluginsField = paperInstanceManager.getClass().getDeclaredField("plugins");
+            pluginsField.setAccessible(true);
+            lookupNamesField = paperInstanceManager.getClass().getDeclaredField("lookupNames");
+            lookupNamesField.setAccessible(true);
+        } catch (Throwable ignored) {
+            paperInstanceManager = null;
+            pluginsField = null;
+            lookupNamesField = null;
+        }
+
+        PAPER_INSTANCE_MANAGER = paperInstanceManager;
+        PLUGINS_FIELD = pluginsField;
+        LOOKUP_NAMES_FIELD = lookupNamesField;
+    }
 
     public static MinecraftReflection getReflection() {
         return reflection;
     }
 
-    public static Map<Pattern, PluginLoader> getFileAssociations(Object manager) throws IllegalAccessException {
-        return reflection.get(manager, "fileAssociations");
-    }
-
+    @SuppressWarnings("unchecked")
     public static List<Plugin> getPlugins(Object manager) {
-        return reflection.get(manager, "plugins");
+        if (PAPER_INSTANCE_MANAGER == null) {
+            return reflection.get(manager, "plugins");
+        }
+
+        try {
+            return (List<Plugin>) PLUGINS_FIELD.get(PAPER_INSTANCE_MANAGER);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -31,8 +64,20 @@ public class RSimplePluginManager {
      * @param manager The SimplePluginManager instance to remove the lookup name from.
      * @param name The name of the plugin to remove.
      */
+    @SuppressWarnings("unchecked")
     public static void removeLookupName(Object manager, String name) {
-        Map<String, Plugin> lookupNames = reflection.get(manager, "lookupNames");
+        Map<String, Plugin> lookupNames;
+
+        if (PAPER_INSTANCE_MANAGER != null) {
+            try {
+                lookupNames = (Map<String, Plugin>) LOOKUP_NAMES_FIELD.get(PAPER_INSTANCE_MANAGER);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            lookupNames = reflection.get(manager, "lookupNames");
+        }
+
         if (lookupNames == null) return;
         lookupNames.remove(name.replace(' ', '_'));
         lookupNames.remove(name.replace(' ', '_').toLowerCase(Locale.ENGLISH)); // Paper
